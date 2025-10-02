db1a76846f72070bad50a2b36bfd3cb541897d64
i-shaurya1912 <vshaurya1912@gmail.com>
2025-10-02 19:35:22 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("8*8") // we are using nodejs, not browser
+
+console.log(3
+    +
+     3) // code readability should be high
+
+console.log("hitesh")
+
+
+let name = "shaurya verma"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =>standalone value
+// undefined => 
+//symbol => unique
+
+
+
+// object
+
+
+console.log(typeof "shaurya")

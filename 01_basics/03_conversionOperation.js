7b59f6c7d59d007522aeba319c115f9da5ea8ae7
i-shaurya1912 <vshaurya1912@gmail.com>
2026-04-13 19:04:12 +0530

    second commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "22abc" =>NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 true => true; 0 => false
+// "" => false
+// "shaurya" => true
+let someNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+

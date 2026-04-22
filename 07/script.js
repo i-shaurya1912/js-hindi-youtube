ce2a9e84a17f1dc6bee7565a5a33d38b0553da92
i-shaurya1912 <vshaurya1912@gmail.com>
2026-04-22 18:49:08 +0530

    first commit

---
+++ 07/script.js
@@ -0,0 +1,34 @@
+//functions
+
+// function sayHello(){
+//     console.log('Hey piyush')
+// }
+
+// //      argument
+// function add(num1 , num2){
+//     return num1 + num2;
+
+// }
+
+// function multiply(a,b){
+
+//     return a * b;
+// }
+
+// let a = add(10 ,9)
+// console.log('That hey user result is ', a)
+
+
+function sayHello(){
+    console.log('hey shaurya')
+
+}
+
+function addEventListener(num1 , num2){
+    console.log(num1 , num2);
+
+}
+
+addEventListener(5 , 10)
+addEventListener(2, 8)
+addEventListener(4,9)
